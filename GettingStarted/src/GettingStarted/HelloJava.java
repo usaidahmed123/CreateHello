bf17fc838ff6306f958af99081f0547e075d9932
jawad <jawadusaid11@gmail.com>
2022-02-11 05:45:47 +0530

    creating project,uploading to git

---
+++ GettingStarted/src/GettingStarted/HelloJava.java
@@ -0,0 +1,18 @@
+
+package GettingStarted;
+import java.util.Scanner;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter whatever u want see in next line!");
+		String rar = s.nextLine();
+		System.out.println("ur output is:"+ rar);
+		System.out.println("Hello and Welcome to Java, Wish you a very happy and Interesting journey learning java");
+		s.close();
+	}
+	
+
+}
