82e6ed3f6fd75c669dde2dc2cd387362f30d99ab
jawad <jawadusaid11@gmail.com>
2022-02-11 05:50:28 +0530

    pushing project, commit

--- GettingStarted/src/GettingStarted/HelloJava.java
+++ GettingStarted/src/GettingStarted/HelloJava.java
@@ -7,7 +7,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		System.out.println("enter whatever u want see in next line!");
+		System.out.println("enter whatever u want to see in the next line!");
 		String rar = s.nextLine();
 		System.out.println("ur output is:"+ rar);
 		System.out.println("Hello and Welcome to Java, Wish you a very happy and Interesting journey learning java");
